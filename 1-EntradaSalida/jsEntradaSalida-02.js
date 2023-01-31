7598512c7782cc2ebf7835544d69903d0f3a27b0
Lucaslk8 <kuliklucaslopez@gmail.com>
2023-01-30 23:34:44 -0300

    guia 1, ej 1 y 2

---
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -0,0 +1,21 @@
+/*
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	// alert("ok");
+
+	// var edad; tratar de usar let en vez de var
+	let nombre;
+	let nombrecompleto; // notacion lower camel case
+
+	 nombre = prompt("ingrese nombre");
+
+	//nombre = "lucas"; // asignacion de string.
+
+	// nombre = 123;
+
+	// mostrar por alert 
+	alert(nombre);
+}
+
